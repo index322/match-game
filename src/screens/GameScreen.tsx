b4440a0a0d5f5f72b35e5b6746408ea819a88069
index322 <danilsavenko98@gmail.com>
2023-07-05 17:17:57 +0300

    added game logic

--- src/screens/GameScreen.tsx
+++ src/screens/GameScreen.tsx
@@ -1,5 +1,12 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React, { FC, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Button,
+  TextInput,
+} from "react-native";
+import React, { FC, useState, useEffect } from "react";
 import { NavigationProp, RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../navigators/GameNavigator";
 
@@ -18,15 +25,73 @@ const GameScreen: FC<GameScreenProps> = ({ navigation, route }) => {
   const [currentUserMatches, setCurrentUserMatches] = useState(0);
   const [computerMatches, setComputerMatches] = useState(0);
   const [userTurn, setUserTurn] = useState(isUserFirst);
+  const [n, setN] = useState(2);
+  const [m, setM] = useState(3);
+
+  const computeMatchesToTake = () => {
+    let matchesToTake = matchesLeft % (m + 1);
+    if (matchesToTake === 0) {
+      matchesToTake = 1;
+    }
+    return matchesToTake;
+  };
+
+  const takeMatches = (matches) => {
+    setUserTurn(!userTurn);
+    setMatchesLeft(matchesLeft - matches);
+
+    if (userTurn) {
+      setCurrentUserMatches(currentUserMatches + matches);
+    } else {
+      setComputerMatches(computerMatches + matches);
+    }
+  };
+
+  useEffect(() => {
+    if (!userTurn) {
+      setTimeout(() => {
+        takeMatches(computeMatchesToTake());
+      }, 1000); // AI takes its turn after 2 seconds
+    }
+  }, [userTurn]);
+
+  useEffect(() => {
+    if (matchesLeft === 0) {
+      const winner = currentUserMatches % 2 === 0 ? "User" : "Computer";
+      alert(`Game Over. ${winner} Wins.`);
+    }
+  }, [matchesLeft]);
 
   const onPress = () => navigation.goBack();
 
   return (
     <View style={styles.container}>
       <View style={styles.circle}>
-        <Text style={styles.headerText}>25</Text>
+        <Text style={styles.headerText}>{matchesLeft}</Text>
       </View>
-
+      <Text>Your Matches: {currentUserMatches}</Text>
+      <Text>Computer's Matches: {computerMatches}</Text>
+      <View>
+        {userTurn && (
+          <View>
+            <Button
+              title="Take 1"
+              onPress={() => takeMatches(1)}
+              disabled={matchesLeft === 0}
+            />
+            <Button
+              title="Take 2"
+              onPress={() => takeMatches(2)}
+              disabled={matchesLeft < 2}
+            />
+            <Button
+              title="Take 3"
+              onPress={() => takeMatches(3)}
+              disabled={matchesLeft < 3}
+            />
+          </View>
+        )}
+      </View>
       <View style={styles.appButtonContainer}>
         <TouchableOpacity>
           <Text style={styles.appButtonText} onPress={onPress}>
@@ -75,6 +140,12 @@ const styles = StyleSheet.create({
     borderColor: "#ffbd03",
     justifyContent: "center",
     alignItems: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
